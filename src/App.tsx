25c35f3999bdd3b07b60444e86c70c18db7d29e6
ghxstling <dcho282@aucklanduni.ac.nz>
2025-03-26 20:32:41 +1300

    feat(wip): various changes to layout, styles and functionality
    - add client state loading to sync with fetch API call
    - add scroll functionality to Header buttons
    - change Header position to fixed
    - add background image to AvatarCard (wip)
    - change slogan and add buttons to AvatarCard (wip)
    - add Card to projects (wip)
    - add Contact component (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
-import { Avatar, Link, Paper, Stack, Tooltip, Typography, Button, Grid2, Container } from '@mui/material'
+import {
+  Avatar,
+  Box,
+  Link,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+  Button,
+  Grid2,
+  Container,
+  CssBaseline,
+  Card,
+} from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
 import { GitHub, LinkedIn, Instagram } from '@mui/icons-material'
 import { theme } from './theme'
 
@@ -11,21 +23,29 @@ const HEADER_MARGIN = 1.5
 const ICON_SIZE = 'large'
 
 export default function App() {
+  const [loaded, setLoaded] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoaded(true)
+  }, [setLoaded])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Stack
-        id="home"
-        sx={{
-          maxWidth: '95vw',
-        }}
-      >
-        <Header />
-        <AvatarCard />
-        <About />
-        <Projects />
-        <Footer />
-      </Stack>
+      {loaded && (
+        <Container component="main">
+          <Box sx={{ my: HEADER_MARGIN * 2 + HEADER_HEIGHT + 'rem' }}>
+            <Header />
+          </Box>
+          <Stack spacing={5}>
+            <AvatarCard />
+            <About />
+            <Projects />
+            <Contact />
+            <Footer />
+          </Stack>
+        </Container>
+      )}
     </ThemeProvider>
   )
 }
@@ -33,23 +53,47 @@ export default function App() {
 function Header() {
   const buttons = ['Home', 'About', 'Projects', 'Contact']
 
+  const handleScrollTo = (id: string) => {
+    const element = document.getElementById(id)
+    if (element) {
+      const rect = element.getBoundingClientRect()
+      let yPos
+      if (element.id === 'home') {
+        yPos = 0
+      } else {
+        yPos = rect.top + window.scrollY - (HEADER_HEIGHT + HEADER_MARGIN) * 16
+      }
+      window.scrollTo({ top: yPos, behavior: 'smooth' })
+    }
+  }
+
   return (
     <Paper
+      id="home"
       component="header"
       elevation={5}
       sx={{
-        position: 'sticky',
-        zIndex: 100,
-        inset: 0,
-        height: HEADER_HEIGHT + 'rem',
+        position: 'fixed',
+        inset: HEADER_MARGIN + 'rem 0 0 50%',
+        transform: 'translate(-50%, -50%)',
         my: HEADER_MARGIN + 'rem',
+        zIndex: 100,
+        height: HEADER_HEIGHT + 'rem',
+        maxWidth: '80rem',
+        width: '100%',
+        minWidth: 'fit-content',
         display: 'flex',
         justifyContent: 'center',
+        borderRadius: 2,
       }}
     >
-      <Grid2 container spacing={2}>
+      <Grid2 container spacing={3}>
         {buttons.map((button) => (
-          <Button key={button} href={'#' + button.toLowerCase()} sx={{ px: '1.5rem' }}>
+          <Button
+            key={button}
+            onClick={() => handleScrollTo(button.toLowerCase())}
+            sx={{ px: '1rem', fontSize: '1.25rem' }}
+          >
             {button}
           </Button>
         ))}
@@ -59,7 +103,7 @@ function Header() {
 }
 
 function AvatarCard() {
-  const AVATAR_SIZE = '15rem'
+  const AVATAR_SIZE = '20rem'
   const FONT_SIZE = '2.5rem'
 
   return (
@@ -68,12 +112,23 @@ function AvatarCard() {
       id="home"
       container
       sx={{
-        backgroundColor: 'green',
         justifyContent: 'center',
         alignItems: 'center',
+        py: '5rem',
         fontSize: FONT_SIZE,
       }}
     >
+      <img
+        src="./assets/code.jpg"
+        alt="Background"
+        style={{
+          position: 'absolute',
+          zIndex: -1,
+          width: '60rem',
+          filter: 'blur(2.5px)',
+          opacity: 0.25,
+        }}
+      />
       <Avatar
         alt="Dylan Choy"
         src="./assets/avatar.jpg"
@@ -86,34 +141,48 @@ function AvatarCard() {
       <Stack
         sx={{
           textAlign: 'left',
-          width: '50%',
-          ml: '3rem',
-          backgroundColor: 'red',
+          ml: '5rem',
+          maxWidth: '25rem',
         }}
-        gap={2}
       >
-        <Typography variant="h1">
+        <Typography variant="h1" mb={1.5}>
           <code>Hey, I'm Dylan.</code>
         </Typography>
-        <Typography>An aspiring Software Engineer who is studying Computer Science / Information Systems</Typography>
+        <Typography
+          fontSize={'1.25rem'}
+          sx={{
+            textWrap: 'wrap',
+          }}
+        >
+          Empowering businesses of tomorrow with software solutions
+        </Typography>
+        <Grid2 container gap={2}>
+          <Link href="#contact">
+            <Button variant="contained">Projects</Button>
+          </Link>
+          <Link href="#contact">
+            <Button variant="contained">Get in Touch</Button>
+          </Link>
+        </Grid2>
       </Stack>
     </Grid2>
   )
 }
 
 function About() {
-  return (<Container
-    id="projects"
-    sx={{
-      bgcolor: 'orange',
-      height: '20rem',
-      py: '1.5rem',
-      textAlign: 'center',
-    }}
-  >
-    <Typography variant="h2">About</Typography>
-    <Typography variant="body1">sadadasdasdasdadsadasdassadasdsadasdasdas</Typography>
-  </Container>)
+  return (
+    <Container
+      id="about"
+      sx={{
+        bgcolor: 'orange',
+        height: '20rem',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h2">About</Typography>
+      <Typography variant="body1">sadadasdasdasdadsadasdassadasdsadasdasdas</Typography>
+    </Container>
+  )
 }
 
 // TODO: Implement Projects component
@@ -126,12 +195,13 @@ function About() {
 function Projects() {
   const [projects, setProjects] = React.useState<any[]>([])
 
+  const CARD_SIZE = '20rem'
+
   React.useEffect(() => {
     async function fetchProjects() {
-      const token = import.meta.env.VITE_GITHUB_PERSONAL_ACCESS_TOKEN
-      const username = import.meta.env.VITE_GITHUB_USERNAME
-
       try {
+        const token = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
+        const username = import.meta.env.VITE_GITHUB_USERNAME
         const response = await fetch(`https://api.github.com/users/${username}/repos`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -159,17 +229,38 @@ function Projects() {
       id="projects"
       sx={{
         bgcolor: 'darkblue',
-        height: '30rem',
-        py: '1.5rem',
         textAlign: 'center',
       }}
     >
       <Typography variant="h2">Projects</Typography>
-      <Stack spacing={2}>
+      <Grid2 container spacing={2} justifyContent={'center'}>
         {projects.map((project) => (
-          <Typography key={project.id}>{project.name}</Typography>
+          <Card
+            key={project.id}
+            variant="outlined"
+            sx={{
+              width: CARD_SIZE,
+              height: CARD_SIZE,
+            }}
+          >
+            <Typography key={project.id}>{project.name}</Typography>
+          </Card>
         ))}
-      </Stack>
+      </Grid2>
+    </Container>
+  )
+}
+
+function Contact() {
+  return (
+    <Container
+      id="contact"
+      sx={{
+        bgcolor: 'purple',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h2">Get in Touch</Typography>
     </Container>
   )
 }
@@ -179,35 +270,35 @@ function Footer() {
     <Container
       component="footer"
       sx={{
-        p: '1.5rem',
+        bgcolor: 'black',
         backgroundColor: 'black',
         justifyItems: 'center',
       }}
     >
-        <Stack direction="row" spacing={1.5}>
-          <Link href="https://github.com/ghxstling" target="_blank">
-            <Tooltip arrow title="GitHub" placement="top">
-              <GitHub fontSize={ICON_SIZE} />
-            </Tooltip>
-          </Link>
-          <Link href="https://www.linkedin.com/in/dylan-choy/" target="_blank">
-            <Tooltip arrow title="LinkedIn" placement="top">
-              <LinkedIn fontSize={ICON_SIZE} />
-            </Tooltip>
-          </Link>
-          <Link href="https://www.instagram.com/doodlyn_" target="_blank">
-            <Tooltip arrow title="Instagram" placement="top">
-              <Instagram fontSize={ICON_SIZE} />
-            </Tooltip>
-          </Link>
-        </Stack>
-        <Grid2>
-          <Typography variant="body2">
-            {'Copyright © '}
-            {new Date().getFullYear()}
-            {' Dylan Choy'}
-          </Typography>
-        </Grid2>
+      <Stack direction="row" spacing={1.5}>
+        <Link href="https://github.com/ghxstling" target="_blank">
+          <Tooltip arrow title="GitHub" placement="top">
+            <GitHub fontSize={ICON_SIZE} />
+          </Tooltip>
+        </Link>
+        <Link href="https://www.linkedin.com/in/dylan-choy/" target="_blank">
+          <Tooltip arrow title="LinkedIn" placement="top">
+            <LinkedIn fontSize={ICON_SIZE} />
+          </Tooltip>
+        </Link>
+        <Link href="https://www.instagram.com/doodlyn_" target="_blank">
+          <Tooltip arrow title="Instagram" placement="top">
+            <Instagram fontSize={ICON_SIZE} />
+          </Tooltip>
+        </Link>
+      </Stack>
+      <Grid2>
+        <Typography variant="body2">
+          {'Copyright © '}
+          {new Date().getFullYear()}
+          {' Dylan Choy'}
+        </Typography>
+      </Grid2>
     </Container>
   )
 }
