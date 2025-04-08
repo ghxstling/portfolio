10ce8f17dd31b604939fff7050660a31a74ea566
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-08 21:07:29 +1200

    feat(projects): implement project details using gihub api

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import {
   FormControl,
   FormHelperText,
   Modal,
+  List,
 } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
 import {
@@ -31,9 +32,13 @@ import {
   SkipNext,
   SkipPrevious,
   FileDownload,
+  Link as LinkIcon,
+  OpenInNew,
 } from '@mui/icons-material'
-import { theme } from './css/theme'
+
 // import nodemailer from 'nodemailer'
+
+import { theme } from './css/theme'
 
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/TextLayer.css'
@@ -339,7 +344,7 @@ function About() {
       id="about"
       sx={{
         height: 'fit-content',
-        px: '3.5rem',
+        px: '3rem',
       }}
     >
       <Stack spacing={5}>
@@ -351,9 +356,9 @@ function About() {
               <Typography textAlign={'left'}>
                 An aspiring Software Developer with a focus on <Bold>backend web development</Bold>. My passion in
                 technology and programming began at a young age, building my first <Bold>Snake</Bold> game using Python
-                in 2018. Since then, I am fueled by a desire to understand the intricacies of software and its potential
-                to provide <Bold>effective solutions</Bold>. I am committed to placing people at the heart of every
-                project, ensuring that my software makes a <Bold>real difference</Bold> in their lives.
+                in 2018. Since then, I am fueled by a <Bold>desire to understand</Bold> the intricacies of software and
+                its potential to provide <Bold>effective solutions</Bold>. I am committed to placing people at the heart
+                of every project, ensuring that my software makes a <Bold>real difference</Bold> in their lives.
               </Typography>
             </Section>
             <Section>
@@ -397,24 +402,16 @@ function About() {
   )
 }
 
-// TODO: add more ProjectData fields
-type ProjectData = {
-  id: number
-  name: string
-  description: string
-  html_url: string
-}
-
 function Projects() {
-  const [projects, setProjects] = React.useState<ProjectData[] | string>('')
-  const projectsToDisplay = {
-    'markit-uoa': 'Markit-UOA',
-    learnquest: 'LearnQuest',
-    'pc-part-hunter': 'PC Part Hunter',
-    'shortr-url': 'Shortr URL',
+  type ProjectData = {
+    id: number
+    name: string
+    html_url: string
+    homepage: string
+    pushed_at: string
   }
 
-  const CARD_SIZE = { lg: '30rem', md: '40rem', sm: '35rem', xs: '30rem' }
+  const [projects, setProjects] = React.useState<ProjectData[] | string>('')
 
   React.useEffect(() => {
     async function fetchProjects() {
@@ -442,33 +439,198 @@ function Projects() {
     fetchProjects()
   }, [])
 
+  const ProjectsList = React.useMemo(() => {
+    const CARD_WIDTH = { lg: '32.5rem', md: '55rem', sm: '45rem' }
+
+    function Bold({ children }: { children: string }) {
+      return (
+        <Typography variant="body2" component="span" sx={{ color: 'primary.light', fontWeight: 'bold' }}>
+          {children}
+        </Typography>
+      )
+    }
+
+    const projectsToDisplay = {
+      'markit-uoa': {
+        displayName: 'Markit-UOA',
+        description:
+          'A Next.js web application that simplifies the marker application process for students and coordinators. Developed in a team of 6 people for a client in UoA as part of CS399 Capstone.',
+        features: [
+          <>
+            Developed a <Bold>RESTful API</Bold> with Next.js equipped with <Bold>role-based authentication</Bold>,
+            authorisation using <Bold>OAuth2</Bold> and <Bold>Zod</Bold> for data validation{' '}
+            <Link
+              href="https://github.com/ghxstling/markit-uoa/blob/master/docs/API_DOCS.md"
+              underline="hover"
+              color="primary.main"
+            >
+              (full docs)
+            </Link>
+          </>,
+          <>
+            Utilised <Bold>Amazon S3</Bold> for storing and serving user-uploaded files, including CVs and transcripts
+          </>,
+          <>
+            Implemented <Bold>SendGrid</Bold> for personalised email notifications to users
+          </>,
+          <>
+            Created a <Bold>custom CSV generator</Bold> for offline access to marker applications
+          </>,
+        ],
+      },
+      learnquest: {
+        displayName: 'LearnQuest',
+        description:
+          'A minimal LMS application built with Next.js that allows for easy online learning and classroom management. This project showcases my frontend skills and ability to create a user-friendly interface.',
+        features: [
+          <>
+            Utilised <Bold>Tailwind CSS</Bold> and <Bold>ShadCN UI</Bold> to create a minimal and responsive UI
+          </>,
+          <>
+            Integrated <Bold>Convex</Bold> with <Bold>Next.js</Bold> for real-time data fetching and state management
+          </>,
+          <>
+            Applied <Bold>good software practices</Bold> by writing clean & maintainable code
+          </>,
+        ],
+      },
+      'pc-part-hunter': {
+        displayName: 'PC Part Hunter (WIP)',
+        description:
+          'An e-commerce/web-scraping Next.js web platform that allows users to search for pricing and stock availability of popular PC parts among NZ retailers. This project demonstrates my full-stack development skills.',
+        features: [
+          <>
+            Utilised <Bold>Playwright</Bold> for web scraping and <Bold>Next.js</Bold> for responsive server-side
+            rendering
+          </>,
+          <>
+            Leveraged <Bold>Tailwind CSS</Bold> and <Bold>ShadCN UI</Bold> for a responsive and modern UI
+          </>,
+          <>
+            Implemented <Bold>dark theme toggling</Bold> with <Bold>Next Themes</Bold> for improved user experience
+          </>,
+        ],
+      },
+      'shortr-url': {
+        displayName: 'Shortr URL',
+        description: (
+          <>
+            A simple URL shortener built with Vite and Material UI that utilises the{' '}
+            <Link href="https://github.com/spoo-me/url-shortener" target="_blank" underline="hover">
+              spoo.me
+            </Link>{' '}
+            API. This project showcases my understanding of RESTful APIs.
+          </>
+        ),
+        features: [
+          <>
+            Built with <Bold>Vite</Bold> for fast development & deployment
+          </>,
+          <>
+            Utilised <Bold>REST API</Bold> and <Bold>fetch() API</Bold> to shorten URLs and retrieve original URLs
+          </>,
+        ],
+      },
+    }
+
+    return Array.isArray(projects) ? (
+      projects.map((project: ProjectData) => {
+        if (!Object.prototype.hasOwnProperty.call(projectsToDisplay, project.name)) return null
+        const projectData = projectsToDisplay[project.name as keyof typeof projectsToDisplay]
+        return (
+          <Card
+            variant="outlined"
+            key={project.id}
+            sx={{
+              width: CARD_WIDTH,
+              height: 'auto',
+              borderRadius: '1rem',
+              position: 'relative',
+              p: '1.5rem',
+              pb: '7rem',
+              transition: 'transform 0.2s ease, box-shadow 0.2s ease',
+              '&:hover': {
+                transform: 'scale(1.025)',
+                boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+              },
+            }}
+          >
+            <Stack
+              spacing={2}
+              sx={{
+                textAlign: 'left',
+              }}
+            >
+              <Link href={project.homepage} target="_blank" underline="hover" color="inherit">
+                <Stack direction={'row'} spacing={1} alignItems={'center'} justifyContent={'center'}>
+                  <Typography variant="h3">{projectData.displayName}</Typography>
+                  <OpenInNew fontSize="medium" />
+                </Stack>
+              </Link>
+              <Box
+                component={'img'}
+                src={`./assets/${project.name}.png`}
+                sx={{
+                  width: '100%',
+                  height: 'auto',
+                  borderRadius: '0.5rem',
+                }}
+              />
+              <Typography variant="body1">{projectData.description}</Typography>
+              <List disablePadding sx={{ listStyleType: 'disc', pl: 3 }}>
+                {projectData.features.map((feature, i) => (
+                  <li key={i}>
+                    <Typography variant="body2" textAlign={'left'}>
+                      {feature}
+                    </Typography>
+                  </li>
+                ))}
+              </List>
+              <Box
+                sx={{
+                  position: 'absolute',
+                  bottom: '1.5rem',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  width: '90%',
+                }}
+              >
+                <Typography variant="body2" textAlign={'left'} pb={1}>
+                  <Bold>Last Updated:</Bold>{' '}
+                  {new Date(project.pushed_at).toLocaleDateString('en-NZ', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true,
+                  })}
+                </Typography>
+                <Link href={project.html_url} target="_blank">
+                  <Button variant="contained" fullWidth>
+                    <Stack direction={'row'} spacing={1}>
+                      <LinkIcon fontSize="medium" />
+                      <Typography>View Project</Typography>
+                    </Stack>
+                  </Button>
+                </Link>
+              </Box>
+            </Stack>
+          </Card>
+        )
+      })
+    ) : (
+      <Typography variant="body1" color="primary.main">
+        {projects}
+      </Typography>
+    )
+  }, [projects])
+
   return (
     <Paper id="projects">
       <Typography variant="h2">Projects</Typography>
-      <Grid2 container spacing={{ lg: 3, md: 3, sm: 3, xs: 1 }} justifyContent={'center'}>
-        {Array.isArray(projects) ? (
-          projects.map((project) => {
-            if (!Object.prototype.hasOwnProperty.call(projectsToDisplay, project.name)) return null
-            const projectName = projectsToDisplay[project.name as keyof typeof projectsToDisplay]
-            return (
-              <Card
-                key={project.id}
-                variant="outlined"
-                sx={{
-                  width: CARD_SIZE,
-                  height: CARD_SIZE,
-                  borderRadius: '0.5rem',
-                }}
-              >
-                <Typography key={project.id}>{projectName}</Typography>
-              </Card>
-            )
-          })
-        ) : (
-          <Typography variant="body1" color="primary.main">
-            {projects}
-          </Typography>
-        )}
+      <Grid2 container spacing={3} justifyContent={'center'}>
+        {ProjectsList}
       </Grid2>
     </Paper>
   )
