9ad13462fec4f9d23b3340517cfa5183c1e83a7b
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-10 19:49:40 +1200

    fix(PDFViewer): update workerSrc to use local asset path and configure static copy for PDF worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
   mode: 'production',
   build: {
     outDir: 'dist',
