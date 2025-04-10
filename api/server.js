717c07ca00d60636e1949829a1f29a661bc1f80b
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-10 16:21:39 +1200

    refactor(server): streamline server configuration and improve API structure

--- api/server.js
+++ api/server.js
@@ -2,37 +2,47 @@ import express from 'express'
 import ViteExpress from 'vite-express'
 import nodemailer from 'nodemailer'
 
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import path from 'path'
 
 import cors from 'cors'
 import compression from 'compression'
 import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
 
 dotenv.config({ path: './.env.local' })
 
 const app = express()
-let port
-if (process.env.NODE_ENV === 'production') {
-  port = 3000
-} else {
-  port = 8000
-}
+const port = 3001
+ViteExpress.config({ mode: 'production' })
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const staticPath = path.join(__dirname, '..', 'dist')
+app.use(express.static(staticPath))
+app.use(express.static('public'))
 app.use(express.json())
-app.use(express.static('public', { maxAge: '1y' }))
-app.use(express.static(join(__dirname, '../dist'), { maxAge: '1y' }))
 app.use(compression())
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://ghxstling.dev'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://ghxstling.dev'],
     methods: ['GET', 'POST'],
   })
 )
 
-app.post('/', (req, res) => {
+app.get('/api', (req, res) => {
+  res.send({
+    message: 'Hello from /api (root)!',
+    status_code: 200,
+  })
+})
+
+app.get('/api/email', (req, res) => {
+  res.send({
+    message: 'Hello from /api/email!',
+    status_code: 200,
+  })
+})
+
+app.post('/api/email', (req, res) => {
   try {
     const { fullName, email, subject, body, phone } = req.body
 
@@ -49,6 +59,7 @@ app.post('/', (req, res) => {
     transporter.verify((error, success) => {
       if (error) {
         console.error('SERVER: Error verifying SMTP server - ', error)
+        throw error
       } else if (success) {
         console.log('SERVER: SMTP server is verified and ready to send messages')
       }
@@ -76,14 +87,13 @@ app.post('/', (req, res) => {
         console.error('SERVER: Error sending email - ', error)
         res.status(400).send({
           status_code: 400,
-          message: 'Error sending email',
           error: error.message,
         })
       } else {
         console.log('SERVER: Email sent - ', info.response)
         res.status(200).send({
           status_code: 200,
-          message: 'Email sent successfully: ' + info.response,
+          message: info.response,
           data: {
             fullName: fullName,
             email: email,
@@ -97,20 +107,17 @@ app.post('/', (req, res) => {
     console.error('SERVER: Error sending email - ', error)
     res.status(400).send({
       status_code: 400,
-      message: 'Error sending email',
       error: error.message,
     })
   }
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, '../dist/index.html'))
+  res.sendFile(path.join(staticPath, 'index.html'))
 })
 
-if (process.env.NODE_ENV === 'production') {
-  app.listen(port, () => console.log(`SERVER: Express.js is listening on port ${port} ...`))
-} else {
-  ViteExpress.listen(app, port, () => {
-    console.log(`SERVER: ViteExpress is running on port ${port} ...`)
-  })
-}
+ViteExpress.listen(app, port, () => {
+  console.log(`SERVER: ViteExpress is running on port ${port} ...`)
+})
+
+export default app
