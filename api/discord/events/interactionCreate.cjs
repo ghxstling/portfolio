b03339baa17f3a985b8a76b6036bb29a75bbbd36
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-27 22:57:23 +1200

    feat(discord-bot): add global error event handler

--- api/discord/events/interactionCreate.cjs
+++ api/discord/events/interactionCreate.cjs
@@ -8,14 +8,16 @@ module.exports = {
     const command = interaction.client.commands.get(interaction.commandName)
 
     if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`)
+      console.error(`SERVER: [ERROR] No command matching ${interaction.commandName} was found.`)
       return
     }
 
     try {
       await command.execute(interaction)
     } catch (error) {
-      console.error(error)
+      console.error(`SERVER: [ERROR] Error executing command ${interaction.commandName}: ${error}`)
+      interaction.client.emit(Events.Error, error)
+
       if (interaction.replied || interaction.deferred) {
         await interaction.followUp({
           content: 'There was an error while executing this command!',
