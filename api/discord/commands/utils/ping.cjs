4e1448fcbc545b70ee160d4935b18ea1ea367f9a
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-27 22:54:49 +1200

    feat(discord-bot): include latency information in ping commmand

--- api/discord/commands/utils/ping.cjs
+++ api/discord/commands/utils/ping.cjs
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!')
+    await interaction.reply(`Pong! Latency: ${interaction.client.ws.ping}ms.`)
   },
 }
