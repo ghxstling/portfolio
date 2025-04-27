6ff8c4aedaf124a6a6d9ca6fab2239f5e7d7716b
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-27 19:38:30 +1200

    feat(discord.js): implement Discord bot with basic commands and event handling

---
+++ api/discord/bot.cjs
@@ -0,0 +1,48 @@
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const dotenv = require('dotenv')
+const fs = require('node:fs')
+const path = require('node:path')
+
+dotenv.config({ path: './.env.local' })
+const token = process.env.DISCORD_BOT_TOKEN
+if (!token) {
+  throw new Error('Missing DISCORD_BOT_TOKEN environment variable')
+}
+
+console.log(`SERVER: Loading Discord bot...`)
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.commands = new Collection()
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.cjs'))
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command)
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.cjs'))
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file)
+  const event = require(filePath)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
+
+client.login(token)
