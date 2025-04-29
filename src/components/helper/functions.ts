264981ed4554c40d54c1182d80ba87533ded34d9
ghxstling <dcho282@aucklanduni.ac.nz>
2025-04-29 15:24:32 +1200

    feat(helper): add parseImageUrl function for image URL parsing

--- src/components/helper/functions.ts
+++ src/components/helper/functions.ts
@@ -21,3 +21,14 @@ export const getApiUrl = (path: string) => {
     return import.meta.env.VITE_API_URL + path
   }
 }
+
+export const parseImageUrl = (image?: string | null): string | null => {
+  if (!image) return null
+  if (image.startsWith('spotify:')) {
+    return `https://i.scdn.co/image/${image.split(':')[1]}`
+  } else if (image.startsWith('mp:external/')) {
+    return `https://${image.split('/').slice(3).join('/')}`
+  } else {
+    return null
+  }
+}
